43e6e7afd05ea93587553d1cef2a5124242db581
Alex <asoulier@gaming.tech>
2026-03-19 21:40:54 +0100

    fix destructor button

--- MiniStudio/EtnaZ-Engine/Button.h
+++ MiniStudio/EtnaZ-Engine/Button.h
@@ -17,7 +17,7 @@ private:
 public:
 	Button() = default;
 	Button(float x, float y, float w, float h, std::vector<GameState*>* states, sf::RenderWindow* window);
-	virtual ~Button() override;
+	virtual ~Button() override = default;
 
 	virtual void update(float& dt, Input& input) override;
 	virtual void render(sf::RenderWindow* window) override;
